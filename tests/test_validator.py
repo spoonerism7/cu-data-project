20812b489fce3c7ef6937cbbaa3e4ec0d5da1aa6
Aldrin Dumas | Technical and Platform Support <aldrin.dumas@genexinstitute.com>
2026-03-31 12:57:32 +0200

    implemented row lenght validation

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -12,3 +12,14 @@ def test_invalid_headers():
 def test_empty_file():
     data = []
     assert validate_not_empty(data) == False
+
+def test_row_length_valid():
+    row = ["1", "12:00"] + ["1.0"] * 10
+    assert validate_row_length(row) == True
+
+def test_row_length_invalid():
+    row = ["1", "12:00"] + ["1.0"] * 5
+    assert validate_row_length(row) == False
+
+def validate_row_length(row):
+    return len(row) == 12
