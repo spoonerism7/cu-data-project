65cb524c729ca64ec3b6e3b2aca1ff20a535ed96
Aldrin Dumas | Technical and Platform Support <aldrin.dumas@genexinstitute.com>
2026-04-02 14:14:42 +0200

    implemented singleton pattern for logger to make sure single shared instance is there

--- src/logger.py
+++ src/logger.py
@@ -1,14 +1,22 @@
 import logging
 import os
 
+_logger = None  # global instance
+
 def setup_logger():
+    global _logger
+
+    if _logger is not None:
+        return _logger  # return existing logger
+
     if not os.path.exists("logs"):
         os.makedirs("logs")
 
     logging.basicConfig(
-        filename = "logs/app.log",
+        filename="logs/app.log",
         level=logging.INFO,
-        format="%(asctime)s - %(levelname)s %(message)s"
+        format="%(asctime)s - %(levelname)s - %(message)s"
     )
 
-    return logging
+    _logger = logging.getLogger("app")
+    return _logger
