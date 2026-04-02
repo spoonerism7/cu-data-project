60ed580991a060e1539ec0b0e305c0c23b0a62f7
Aldrin Dumas | Technical and Platform Support <aldrin.dumas@genexinstitute.com>
2026-04-02 11:17:34 +0200

    added validate_file function to combine all validation checks

--- src/validator.py
+++ src/validator.py
@@ -18,3 +18,32 @@ def validate_unique_batch_ids(rows):
 
 def validate_readings(readings):
     return all(0 <= float(r) <= 9.9 for r in readings)
+
+def validate_file(data):
+    # Check not empty
+    if not validate_not_empty(data):
+        return False, "File is empty"
+    
+    headers = data[0]
+    rows = data[1:]
+
+    # check headers
+    if not validate_headers(headers):
+        return False, "Invalid headers"
+    
+    #check row in rows:
+    for row in rows:
+        if not validate_row_length(row):
+            return False, "Invalid row length"
+    
+    # check unique batch IDs
+    if not validate_unique_batch_ids(dict_rows):
+        return False, "duplicate batch IDs found"
+    
+    # check readings
+    for row in rows:
+        readings = row[2:]
+        if not validate_readings(readings):
+            return False, "Invalid reading values"
+        
+    return True, "File is valid"
